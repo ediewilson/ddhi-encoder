843500b6361b31c1d6fd4540caf9d52a1fed84ac
Cliff Wulfman <cwulfman@gmail.com>
2020-08-24 19:29:13 -0400

    working on transformer module

---
+++ tests/transformer/test_transformer.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import os
+#from ddhi_encoder.transformer import Transformer
+from ddhi_encoder.transformer.transformer import Transformer
+from lxml import etree
+
+
+thunk = os.path.join(os.path.dirname(__file__), 'test.xsl')
+test_xml = os.path.join(os.path.dirname(__file__), "test.xml")
+
+def test_xsl():
+    subject = Transformer()
+    assert type(subject).__name__ == "Transformer"
+    output = subject.transform_with_xsl(thunk, test_xml)
+    assert "<?xml" in str(output)
+
